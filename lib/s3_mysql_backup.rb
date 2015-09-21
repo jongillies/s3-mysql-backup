46b5789d893df5cd548cd50edf3bca04b53608b7
Jonathan Gillies <jgillies@skytouchtechnology.com>
2015-09-21 14:29:56 -0700

    added .idea

--- lib/s3_mysql_backup.rb
+++ lib/s3_mysql_backup.rb
@@ -89,16 +89,8 @@ class S3MysqlBackup
     content << "\n#{File.basename(filename)}\n" # body
     content = content.join("\n")
 
-    smtp = Net::SMTP.new(config["mail_domain"], config["mail_port"])
-    smtp.enable_starttls unless config["mail_start_tls"] == false
-    smtp.start(
-               config["mail_domain"].to_s,
-               config['mail_user'].to_s,
-               config['mail_pass'].to_s,
-               config['mail_authentication'].to_s
-               ) do
-      smtp.send_message(content, mail_from, config['mail_to'])
-    end
+    smtp = Net::SMTP.new 'localhost'
+    smtp.send_message(content, mail_from, config['mail_to'])
   end
 
   # remove old backups
